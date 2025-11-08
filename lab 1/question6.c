06b73b3cb08d29c9b095716f5166abf07e01d49c
Nishan Shrestha <bluephenoix3@gmail.com>
2025-11-08 10:01:21 +0545

    update

---
+++ lab 1/question6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float L,B,A;
+	printf("Write the Length and Breath of the rectangle:\n");
+	scanf("%f %f",&L,&B);
+	A = L * B;
+	printf("The area of the rectangle is %f\n",A);
+}
