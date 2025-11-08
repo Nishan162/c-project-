166e2cb0b0defeb469e646c81620db5543987f2b
Nishan Shrestha <bluephenoix3@gmail.com>
2025-11-08 10:18:45 +0545

    update

--- lab 2/question2.c
+++ lab 2/question2.c
@@ -1,19 +1,25 @@
-#include <stdio.h>
-
-int main() {
-    int number;
-    printf("Enter an integer: ");
-    scanf("%d", &number);
-    if (number % 5 == 0) {
-        printf("%d is a multiple of 5.\n", number);
-    } else {
-        printf("%d is NOT a multiple of 5.\n", number);
-    }
-    if (number % 7 == 0 && number % 11 != 0) {
-        printf("%d is divisible by 7 but NOT by 11.\n", number);
-    } else {
-        printf("%d does NOT satisfy 'divisible by 7 but not by 11'.\n", number);
-    }
-
-    return 0;
+#include<stdio.h>
+#include<conio.h>
+int main(){
+  char ch;
+  int ascci;
+  printf("Enter any character?\n");
+  ch = getch();
+  putch(ch);
+  ascci = (int)ch;
+  if(ascci >= 97 && ascci <= 122){
+    printf(" is Small alphabet");
+  } else if(ascci >= 65 && ascci <= 90){
+    printf(" is Capital alphabet");
+  } else if(ascci >= 48 && ascci <= 57){
+    printf(" is Digit");
+  }else if((ascci >= 0 && ascci <= 47) || 
+  (ascci >= 58 && ascci <= 64) || 
+  (ascci >= 91 && ascci <= 96) || 
+  (ascci >= 123 && ascci <= 127)){
+    printf(" is Special Symbol");
+  }  else {
+    printf(" is Invalid Character");
+  }
+  return 0;
 }
