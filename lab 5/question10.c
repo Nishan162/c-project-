d935db4ea655e8600666cd46d7f14a7d9abab066
Nishan Shrestha <bluephenoix3@gmail.com>
2025-11-08 13:52:19 +0545

    update

---
+++ lab 5/question10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void swap(int a, int b);
+
+int main() {
+    int x = 10, y = 20;
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+    swap(x, y);
+
+    printf("After swapping (in main): x = %d, y = %d\n", x, y);
+
+    return 0;
+}
+void swap(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("Inside swap function: a = %d, b = %d\n", a, b);
+}
