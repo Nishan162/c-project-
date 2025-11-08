ea094a8837b12e37355bb1b98f41463195ea5e70
Nishan Shrestha <bluephenoix3@gmail.com>
2025-11-08 13:24:21 +0545

    update

---
+++ lab 4/question11.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    char str1[] = {'A', 'n', 'i', 's', 'h', '\0'};
+    char str2[] = "Gautam";
+    printf("String 1: %s\n", str1);
+    printf("String 2: %s\n", str2);
+
+    return 0;
+}
